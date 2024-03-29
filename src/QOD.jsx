52fd72c00be831a09b4aa55a6dc637005351fe82
coortoise <curtis2002@hotmail.ca>
2024-03-28 20:56:50 -0400

    added kitchen safety questions

--- src/QOD.jsx
+++ src/QOD.jsx
@@ -4,8 +4,99 @@ import { Model } from 'survey-core';
 import { Survey } from 'survey-react-ui';
 
 export default function () {
+    const questions = [{
+                type: "radiogroup",
+                name: "question1",
+                title: "What is most important factor in food safety?",
+                choices: [
+                    "Cleanliness", "Organization", "Profit", "Communication"
+                ],
+                correctAnswer: "Cleanliness"
+            },
+            {
+                type: "radiogroup",
+                name: "question2",
+                title: "Which foods require extra caution?",
+                choices: [
+                    "Undercooked meats and eggs", "Oystes and sprouts", "Unpasteurized milk or juices", "All of the above"
+                ],
+                correctAnswer: "All of the above"
+            },
+            {
+                type: "radiogroup",
+                name: "question3",
+                title: "What are you protecting when using gloves?",
+                choices: [
+                    "Your hands from food", "Your hands from germs", "Your food from germs", "Your food from hands"
+                ],
+                correctAnswer: "Your food from germs"
+            },
+            {
+                type: "radiogroup",
+                name: "question4",
+                title: "What is the most common food hazard?",
+                choices: [
+                    "Physical", "Biological", "Chemical", "Miscommunication"
+                ],
+                correctAnswer: "Biological"
+            },
+            {
+                type: "radiogroup",
+                name: "question5",
+                title: "Use a seperate cutting board for:",
+                choices: [
+                    "Raw meat", "Vegetables", "Seafood", "All of the above"
+                ],
+                correctAnswer: "All of the above"
+            },
+            {
+                type: "radiogroup",
+                name: "question6",
+                title: "Always wash your hands when:",
+                choices: [
+                    "Handling raw meat", "Using the bathroom", "Touching your face or nose", "All of the above"
+                ],
+                correctAnswer: "All of the above"
+            },
+            {
+                type: "radiogroup",
+                name: "question7",
+                title: "What is an example of a physical hazard?",
+                choices: [
+                    "Bacteria", "Mold", "Broken glass", "Chemicals"
+                ],
+                correctAnswer: "Broken glass"
+            },
+            {
+                type: "radiogroup",
+                name: "question8",
+                title: "How often should you wash food contact surfaces?",
+                choices: [
+                    "Once a day", "Once a week", "After each use", "Only when it looks dirty"
+                ],
+                correctAnswer: "After each use"
+            },
+            {
+                type: "radiogroup",
+                name: "question9",
+                title: "Which of the following is a common food allergen?",
+                choices: [
+                    "Salt", "Water", "Nuts", "Garlic"
+                ],
+                correctAnswer: "Nuts"
+            },
+            {
+                type: "radiogroup",
+                name: "question10",
+                title: "What food is the most dangerous if not fully cooked?",
+                choices: [
+                    "Beef", "Vegetables", "Chicken", "Fruits"
+                ],
+                correctAnswer: "Chicken"
+            }];
+    const nQuestion = Math.floor((Math.random() * questions.length));
     const surveyJson = {
-        title: "Lifeguard Safety",
+        title: "Kitchen Safety",
         showCorrectAnswer: "always",
         showProgressBar: "bottom",
         firstPageIsStarted: true,
@@ -13,7 +104,7 @@ export default function () {
         pages: [{
             elements: [{
                 type: "html",
-                html: "You are about to start a quiz on Lifeguard Safety. <br>You will have 30 seconds for every question and 60 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
+                html: "You are about to start a quiz on Kitchen Safety. <br>You will have 30 seconds for every question and 60 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
             }, {
                 type: "text",
                 name: "username",
@@ -21,15 +112,7 @@ export default function () {
                 isRequired: true
             }]
         }, {
-            elements: [{
-                type: "radiogroup",
-                name: "question1",
-                title: "What is the most common cause of lifeguard injuries?",
-                choices: [
-                    "Slip and fall accidents", "Pool chemicals", "Sunburns", "Sharks"
-                ],
-                correctAnswer: "Slip and fall accidents"
-            }]
+            elements: [questions[nQuestion]]
         }]
     };
     const survey = new Model(surveyJson);
